018341f736e5fc16f7c1b501b7bc020bb248e062
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-03-29 15:43:18 +0300

    правки

--- hw-01-testing-app/src/main/java/ru/otus/dao/QuestionDaoImpl.java
+++ hw-01-testing-app/src/main/java/ru/otus/dao/QuestionDaoImpl.java
@@ -1,45 +1,64 @@
 package ru.otus.dao;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import lombok.Getter;
 import ru.otus.domain.Answer;
 import ru.otus.domain.Question;
-import ru.otus.utils.CsvParser;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 public class QuestionDaoImpl implements QuestionDao {
 
-    private static final String SEPARATOR = ";";
-
-    private final CsvParser csvParser;
-
     private final String nameFile;
 
-    public QuestionDaoImpl(CsvParser csvParser, String nameFile) {
-        this.csvParser = csvParser;
+    public QuestionDaoImpl(String nameFile) {
         this.nameFile = nameFile;
     }
 
+    @Override
     public List<Question> getQuestionList() {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(nameFile);
-        return csvParser.readLinesItems(inputStream, SEPARATOR, true).stream()
-                .map(item -> {
-                    Long id = Long.valueOf(item.get(0));
-                    String title = item.get(1);
-                    String text = item.get(2);
-                    String correctAnswer = item.get(3);
-                    List<Answer> answers = new ArrayList<>();
-                    for (int i = 4; i < item.size(); i++) {
-                        String value = item.get(i);
-                        var answer = new Answer(value, correctAnswer.equalsIgnoreCase(value));
-                        answers.add(answer);
-                    }
-                    return new Question(id, title, text, answers);
-                }
-        ).collect(Collectors.toList());
+        System.out.println(nameFile);
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(nameFile)) {
+            return readLinesItems(inputStream).stream()
+                    .skip(1)
+                    .map(item -> {
+                                Long id = Long.valueOf(item.get(0));
+                                String title = item.get(1);
+                                String text = item.get(2);
+                                String correctAnswer = item.get(3);
+                                List<Answer> answers = new ArrayList<>();
+                                for (int i = 4; i < item.size(); i++) {
+                                    String value = item.get(i);
+                                    var answer = new Answer(value, correctAnswer.equalsIgnoreCase(value));
+                                    answers.add(answer);
+                                }
+                                return new Question(id, title, text, answers);
+                            }
+                    ).collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("Missing list of questions", e);
+        }
+
+    }
+
+    private List<List<String>> readLinesItems(InputStream inputStream) {
+        List<List<String>> records = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream))) {
+            String[] values;
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+        } catch (CsvValidationException | IOException e) {
+            throw new RuntimeException("Wrong file structure");
+        }
+        return records;
     }
 }
--- hw-01-testing-app/src/main/java/ru/otus/service/TestingServiceImpl.java
+++ hw-01-testing-app/src/main/java/ru/otus/service/TestingServiceImpl.java
@@ -16,26 +16,27 @@ public class TestingServiceImpl implements TestingService {
         this.questionDao = questionDao;
     }
 
+    @Override
     public void testing() {
         List<Question> questionList = questionDao.getQuestionList();
         Scanner in = new Scanner(System.in);
         successCount = 0;
 
         System.out.println("Hello, welcome to the testing program!");
-        System.out.println("You have to answer "+questionList.size()+" questions");
+        System.out.println("You have to answer " + questionList.size() + " questions");
 
         for (Question question : questionList) {
-            System.out.println("\nQuestion №"+question.getId()+" - "+question.getTitle());
+            System.out.println("\nQuestion №" + question.getId() + " - " + question.getTitle());
             System.out.println(question.getText());
             System.out.println("Answer options (select one):");
-            question.getAnswersValue().forEach(el -> System.out.println("\t"+el));
+            question.getAnswersValue().forEach(el -> System.out.println("\t" + el));
             System.out.print("Enter your answer: ");
             String answer = in.next();
             System.out.println(check(answer, question));
         }
 
         System.out.println("---------------------------------------------------");
-        System.out.println("you answered "+successCount+" questions out of "+questionList.size()+" correctly");
+        System.out.println("you answered " + successCount + " questions out of " + questionList.size() + " correctly");
     }
 
     private boolean check(String answer, Question question) {
