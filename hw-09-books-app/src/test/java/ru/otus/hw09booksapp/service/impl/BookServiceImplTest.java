e012c03cd6dedf22e9e0c045e72373f7354d8da5
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-06-04 16:42:50 +0300

    добавил тесты для сервисов

---
+++ hw-09-books-app/src/test/java/ru/otus/hw09booksapp/service/impl/BookServiceImplTest.java
@@ -0,0 +1,81 @@
+package ru.otus.hw09booksapp.service.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw09booksapp.dto.BookDto;
+import ru.otus.hw09booksapp.entity.Author;
+import ru.otus.hw09booksapp.entity.Book;
+import ru.otus.hw09booksapp.entity.Genre;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DisplayName("ORM JPA books repository testing.")
+@DataJpaTest
+@Import({BookServiceImpl.class,
+        AuthorServiceImpl.class,
+        GenreServiceImpl.class})
+class BookServiceImplTest {
+
+    private final static int EXPECTED_BOOKS_COUNT = 1;
+    private final static Author AUTHOR_ONE = new Author(1L, "Михаил Булгаков");
+    private final static Genre GENRE_ONE = new Genre(1L, "Roman");
+    private final static String BOOK_ONE_NAME = "MasterTest";
+    private final static Book BOOK_ONE = new Book(1L, AUTHOR_ONE, GENRE_ONE, BOOK_ONE_NAME);
+
+    @Autowired
+    private BookServiceImpl bookService;
+
+    @DisplayName("Should get correct book")
+    @Test
+    void shouldGetCorrectBook() {
+        Book book = bookService.getById(1L);
+        assertThat(book).isNotNull()
+                .matches(b -> Objects.equals(b.getId(), BOOK_ONE.getId()))
+                .matches(b -> b.getTitle().equals(BOOK_ONE.getTitle()));
+    }
+
+    @DisplayName("Should find all books")
+    @Test
+    void ShouldFindAllBooks() {
+        List<Book> books = bookService.getAll();
+        assertEquals(EXPECTED_BOOKS_COUNT, books.size());
+    }
+
+    @DisplayName("Should get correct books count")
+    @Test
+    void ShouldGetCorrectBooksCount() {
+        assertEquals(EXPECTED_BOOKS_COUNT, bookService.getCount());
+    }
+
+    @DisplayName("Should be able to delete a book:")
+    @Test
+    void shouldDeletefirstBook() {
+        Book book = bookService.getById(1L);
+        assertEquals(BOOK_ONE_NAME, book.getTitle());
+        // DELETE:
+        bookService.deleteById(1L);
+        assertThatCode(() -> bookService.getById(1L))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @DisplayName("Should be able to insert new book-1")
+    @Test
+    void shouldInsertNewBook() {
+        Book book = new Book(null, new Author(1L, "test"), new Genre(1L, "Roman"), "MasterTest");
+        Book savedBook = bookService.create(new BookDto(null, "MasterTest", "test", "Roman"));
+        assertThat(savedBook.getId()).isGreaterThan(0);
+        assertEquals(book.getAuthor().getName(), savedBook.getAuthor().getName());
+        assertEquals(book.getGenre().getName(), savedBook.getGenre().getName());
+        assertEquals(book.getTitle(), savedBook.getTitle());
+    }
+
+}
---
+++ hw-09-books-app/src/test/java/ru/otus/hw09booksapp/service/impl/NoteServiceImplTest.java
@@ -0,0 +1,76 @@
+package ru.otus.hw09booksapp.service.impl;
+
+import lombok.val;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.otus.hw09booksapp.entity.Author;
+import ru.otus.hw09booksapp.entity.Book;
+import ru.otus.hw09booksapp.entity.Genre;
+import ru.otus.hw09booksapp.entity.Note;
+import ru.otus.hw09booksapp.repository.NoteRepository;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@DisplayName("ORM JPA Genres repository testing.")
+@DataJpaTest
+class NoteServiceImplTest {
+
+    private final static int EXPECTED_NOTES_COUNT = 1;
+    private final static long NOTE_ONE_ID = 1L;
+    private final static String NOTE_ONE_CONTEXT = "Note-01.1 - Master";
+    private final static String NOTE_ONE_CONTEXT_NEW = "Note-01.1 - Master - New";
+    private final static Author AUTHOR_ONE = new Author(1L, "Михаил Булгаков");
+    private final static Genre GENRE_ONE = new Genre(1L, "Roman");
+    private final static String BOOK_ONE_NAME = "MasterTest";
+    private final static Book BOOK_ONE = new Book(1L, AUTHOR_ONE, GENRE_ONE, BOOK_ONE_NAME);
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+
+    @DisplayName("Should get correct Note by ID")
+    @Test
+    void shouldGetCorrectGenre() {
+        Optional<Note> genre = noteRepository.findById(NOTE_ONE_ID);
+        assertEquals(NOTE_ONE_ID, genre.get().getId());
+        assertEquals(NOTE_ONE_CONTEXT, genre.get().getNote());
+    }
+
+    @DisplayName("Should find all Note")
+    @Test
+    void ShouldGetAllGenres() {
+        val genres = noteRepository.findAllBy();
+        assertThat(genres).isNotNull().hasSize(EXPECTED_NOTES_COUNT)
+                .allMatch(s -> s.getId() > 0)
+                .allMatch(s -> !s.getNote().equals(""));
+    }
+
+    @DisplayName("Should be able to delete a Genre:")
+    @Test
+    void shouldDeletefirstGenre() {
+        Optional<Note> note = noteRepository.findById(NOTE_ONE_ID);
+        assertNotNull(note.get());
+        assertEquals(NOTE_ONE_CONTEXT, note.get().getNote());
+        // DELETE:
+        noteRepository.delete(note.get());
+        Optional<Note> noteAfterDelete = noteRepository.findById(NOTE_ONE_ID);
+        assertEquals(Optional.empty(), noteAfterDelete);
+    }
+
+    @DisplayName("Should be able to insert a Genre-1 after deletions")
+    @Test
+    void shouldAddNewGenre() {
+        Note note = new Note(0L, BOOK_ONE, NOTE_ONE_CONTEXT_NEW);
+        Note savedNote = noteRepository.save(note);
+        assertThat(savedNote.getId()).isGreaterThan(0);
+        assertEquals(NOTE_ONE_CONTEXT_NEW, savedNote.getNote());
+    }
+
+}
