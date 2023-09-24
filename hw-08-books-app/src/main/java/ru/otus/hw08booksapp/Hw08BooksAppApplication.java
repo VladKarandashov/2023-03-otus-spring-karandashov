37af88bd65ffd042d5bc19995744c5e13ec979f2
VladKarandashov <vlad.karandashov.tech@gmail.com>
2023-09-24 10:49:24 +0300

    создал проект

---
+++ hw-08-books-app/src/main/java/ru/otus/hw08booksapp/Hw08BooksAppApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw08booksapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw08BooksAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw08BooksAppApplication.class, args);
+    }
+
+}
