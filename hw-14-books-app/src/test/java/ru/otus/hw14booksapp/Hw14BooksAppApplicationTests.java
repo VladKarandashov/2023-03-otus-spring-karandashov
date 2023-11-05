29cd791b62922185d51bb8b0c294f693e61e1503
karandashov <vlad.karandashov.tech@gmail.com>
2023-11-05 15:47:10 +0300

    починил тест

--- hw-14-books-app/src/test/java/ru/otus/hw14booksapp/Hw14BooksAppApplicationTests.java
+++ hw-14-books-app/src/test/java/ru/otus/hw14booksapp/Hw14BooksAppApplicationTests.java
@@ -3,7 +3,7 @@ package ru.otus.hw14booksapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Hw14BooksAppApplicationTests.class)
 class Hw14BooksAppApplicationTests {
 
     @Test
