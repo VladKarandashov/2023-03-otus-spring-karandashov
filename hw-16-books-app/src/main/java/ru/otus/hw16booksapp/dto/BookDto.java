c75641e2c112ec74cb7a2558a62a92d22984e7ff
karandashov <vlad.karandashov.tech@gmail.com>
2023-11-25 17:22:22 +0300

    добавил контейнер тесты

--- hw-16-books-app/src/main/java/ru/otus/hw16booksapp/dto/BookDto.java
+++ hw-16-books-app/src/main/java/ru/otus/hw16booksapp/dto/BookDto.java
@@ -4,8 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class BookDto {
     private Long id;
--- hw-16-books-app/src/test/java/ru/otus/hw16booksapp/TestHw16BooksAppApplication.java
+++ hw-16-books-app/src/test/java/ru/otus/hw16booksapp/TestHw16BooksAppApplication.java
@@ -13,7 +13,7 @@ public class TestHw16BooksAppApplication {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:13"));
 	}
 
 	public static void main(String[] args) {
