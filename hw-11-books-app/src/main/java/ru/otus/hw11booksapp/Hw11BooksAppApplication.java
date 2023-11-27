0df30216a279de914ea1f9cf953dbb775f2713bf
karandashov <vlad.karandashov.tech@gmail.com>
2023-11-27 19:44:38 +0300

    потратил кучу нервов, но смог заставить работать mongock

--- hw-11-books-app/src/main/java/ru/otus/hw11booksapp/Hw11BooksAppApplication.java
+++ hw-11-books-app/src/main/java/ru/otus/hw11booksapp/Hw11BooksAppApplication.java
@@ -1,9 +1,13 @@
 package ru.otus.hw11booksapp;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+@EnableMongock
+@EnableMongoRepositories
 @EnableReactiveMongoRepositories
 @SpringBootApplication
 public class Hw11BooksAppApplication {
--- hw-11-books-app/src/main/java/ru/otus/hw11booksapp/changelog/DatabaseChangelog.java
+++ hw-11-books-app/src/main/java/ru/otus/hw11booksapp/changelog/DatabaseChangelog.java
@@ -1,38 +1,34 @@
 package ru.otus.hw11booksapp.changelog;
 
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.otus.hw11booksapp.entity.Author;
 import ru.otus.hw11booksapp.entity.Book;
 import ru.otus.hw11booksapp.entity.Genre;
-import ru.otus.hw11booksapp.repository.BookRepository;
+import ru.otus.hw11booksapp.repository.BookNonReactiveRepository;
 
-@Service
-@RequiredArgsConstructor
+@Component
+@ChangeLog
+@NoArgsConstructor
 public class DatabaseChangelog {
 
-    private final BookRepository bookRepository;
-
-    @PostConstruct
-    public void init() {
-        dropDb();
-        insertRecords();
+    @ChangeSet(order = "001", id = "dropDb", author = "vlad", runAlways = true)
+    public void dropDb(BookNonReactiveRepository bookRepository) {
+        bookRepository.deleteAll();
     }
 
-    public void dropDb() {
-        bookRepository.deleteAll().subscribe();
-    }
+    @ChangeSet(order = "002", id = "insertRecords", author = "vlad", runAlways = true)
+    public void insertRecords(BookNonReactiveRepository bookRepository) {
 
-    public void insertRecords() {
-
-        var book1 = bookRepository.save(new Book("Мастер и Маргарита", new Author("Михаил Булгаков"), new Genre("Роман"))).subscribe();
-        var book2 = bookRepository.save(new Book("Собачье сердце", new Author("Михаил Булгаков"), new Genre("Повесть"))).subscribe();
-        var book3 = bookRepository.save(new Book("Маленький принц", new Author("Антуан де Сент-Экзюпери"), new Genre("Сказка"))).subscribe();
-        var book4 = bookRepository.save(new Book("Планета людей", new Author("Антуан де Сент-Экзюпери"), new Genre("Сборник"))).subscribe();
-        var book5 = bookRepository.save(new Book("Граф Монте-Кристо", new Author("Александр Дюма"), new Genre("Роман"))).subscribe();
-        var book6 = bookRepository.save(new Book("Три мушкетёра", new Author("Александр Дюма"), new Genre("Роман"))).subscribe();
-        var book7 = bookRepository.save(new Book("Трудно быть богом", new Author("Аркадий и Борис Стругацкие"), new Genre("Фантастика"))).subscribe();
-        var book8 = bookRepository.save(new Book("Понедельник начинается в субботу", new Author("Аркадий и Борис Стругацкие"), new Genre("Фантастика"))).subscribe();
+        bookRepository.save(new Book("Мастер и Маргарита", new Author("Михаил Булгаков"), new Genre("Роман")));
+        bookRepository.save(new Book("Собачье сердце", new Author("Михаил Булгаков"), new Genre("Повесть")));
+        bookRepository.save(new Book("Маленький принц", new Author("Антуан де Сент-Экзюпери"), new Genre("Сказка")));
+        bookRepository.save(new Book("Планета людей", new Author("Антуан де Сент-Экзюпери"), new Genre("Сборник")));
+        bookRepository.save(new Book("Граф Монте-Кристо", new Author("Александр Дюма"), new Genre("Роман")));
+        bookRepository.save(new Book("Три мушкетёра", new Author("Александр Дюма"), new Genre("Роман")));
+        bookRepository.save(new Book("Трудно быть богом", new Author("Аркадий и Борис Стругацкие"), new Genre("Фантастика")));
+        bookRepository.save(new Book("Понедельник начинается в субботу", new Author("Аркадий и Борис Стругацкие"), new Genre("Фантастика")));
     }
 }
