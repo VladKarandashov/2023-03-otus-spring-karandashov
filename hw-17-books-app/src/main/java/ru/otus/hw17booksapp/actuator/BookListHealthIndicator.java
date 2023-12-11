6306302ffbe3d7adb89b7c2a843cc4c22182c325
karandashov <vlad.karandashov.tech@gmail.com>
2023-12-11 19:03:40 +0300

    использовал в healthcheck репозиторий напрямую + обработал возможную ошибку

--- hw-17-books-app/src/main/java/ru/otus/hw17booksapp/actuator/BookListHealthIndicator.java
+++ hw-17-books-app/src/main/java/ru/otus/hw17booksapp/actuator/BookListHealthIndicator.java
@@ -1,30 +1,33 @@
 package ru.otus.hw17booksapp.actuator;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
-import ru.otus.hw17booksapp.service.BookService;
+import ru.otus.hw17booksapp.repository.BookRepository;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BookListHealthIndicator implements HealthIndicator {
 
-    private final BookService bookService;
+    private final BookRepository bookRepository;
 
     @Override
     public Health health() {
-        boolean isServerDown = bookService.getAll().isEmpty();
-        if (isServerDown) {
-            return Health.down()
-                    .status(Status.DOWN)
+
+        try {
+            long booksCount = bookRepository.count();
+            return booksCount == 0 ?
+                    Health.down().status(Status.DOWN).withDetail("message", "Количество книг равно нулю!").build() :
+                    Health.up().status(Status.UP).withDetail("message", "Количество книг больше 0").build();
+        } catch (Exception e) {
+            log.error("Произошла ошибка при попытке подсчитать количество книг в БД", e);
+            return Health.down().status(Status.DOWN)
                     .withDetail("message", "Количество книг равно нулю!")
-                    .build();
-        } else {
-            return Health.up()
-                    .status(Status.UP)
-                    .withDetail("message", "Количество книг больше 0")
+                    .withException(e)
                     .build();
         }
     }
